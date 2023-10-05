fc14af964095b95573dad8da6b78cd721dafe51b
ismenai <144688780+ismenai@users.noreply.github.com>
2023-10-06 00:21:42 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Designer", "Code Developer","Apps/Website Design","Creator Video","Film Editor"],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
